84c3d9783f901dbb65b3c3ebccad697d633c833f
Sumon <Sumon@192.168.2.15>
2023-07-27 09:14:00 +0600

    javamail sender

---
+++ src/main/java/com/SpingBootMailSenderApp/SpingBootMailSenderAppApplication.java
@@ -0,0 +1,13 @@
+package com.SpingBootMailSenderApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootMailSenderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootMailSenderAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpingBootMailSenderApp/SpingBootMailSenderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpingBootMailSenderApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootMailSenderAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
